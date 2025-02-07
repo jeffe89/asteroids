340986e3338cb5135cb614d42b0bbe808c7eb489
jeffe89 <jeffe8989@gmail.com>
2025-02-06 19:17:43 -0800

    Code now generates GUI

--- main.py
+++ main.py
@@ -2,9 +2,33 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    #Initialize pygame
+    pygame.init()
+
+    #Welcome Message
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    #Initilize GUI Screen with predefined constant variables for width / height
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #Setup game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+
+        #Fill screen with solid black
+        screen.fill((0, 0, 0))
+	
+	#Refresh screen
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
